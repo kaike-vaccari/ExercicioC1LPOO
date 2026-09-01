13196de113928363ab64865b3c8194d065d80f3b
WerneyPark <psnwerneypark@gmail.com>
2026-09-01 15:33:13 -0300

    ITEM 1 FEITO
    CRIADOS:
    -Método para preencher uma lista com números aleatórios.
    -Método para criar uma lista sem números repetidos a partir de outra lista.
    -Método para verificar se o numero já existe em uma lista.

--- src/mainarray/Main.java
+++ src/mainarray/Main.java
@@ -1,8 +1,13 @@
 package mainarray;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
+	
+	static Scanner scan = new Scanner(System.in); // precisa estar aqui para caso algum metodo precise solicitar ao usuario
+
 
 	public static void main(String[] args) {
 //		Faça um programa que trabalhe com um ArrayList. O programa deverá
@@ -12,7 +17,24 @@ public class Main {
 //		3 - Contar ocorrência de um valor
 //		4 - Intercalar duas listas
 //		5 - Sair
-		Scanner scan = new Scanner(System.in);
+		//Scanner scan = new Scanner(System.in); TROCADO DE LUGAR
+		
+		
+		ArrayList<Integer> lista1 = new ArrayList<>();
+		ArrayList<Integer> lista2 = new ArrayList<>();
+		
+		//Item 1
+		System.out.println("Bem vindo, informe a quantidade de elementos da lista 1:");
+		int num  = scan.nextInt();
+				
+		lista1 = preencher(num);
+		System.out.println("valores incluidos na lista: " + lista1);
+				
+		lista2 = novaListaSemRepetido(lista1);
+		System.out.println("valores da lista sem repetição: " + lista2);
+		//FIM ITEM 1
+		
+		
 		int opcao;
 		
 		do {
@@ -48,6 +70,44 @@ public class Main {
 			System.out.println("Opção inválida! Digite novamente");
 			break;
 		}
+	} // FIM MAIN
+	
+	//--------------------------------------------------------------
+	
+	
+	
+	private static ArrayList<Integer> novaListaSemRepetido(ArrayList<Integer> lista1) {
+		ArrayList<Integer> novaLista = new ArrayList<>();
+		
+		for (int i=0; i < lista1.size();i++) {
+			int valor = lista1.get(i);
+			if(!valorExiste(novaLista, valor)) { // verifica se o valor ja esta na lista, se não estiver adiciona
+				novaLista.add(lista1.get(i));
+			}
+		}
+		
+		
+		return novaLista;
 	}
 
-}
+	private static boolean valorExiste(ArrayList<Integer> lista, int valor) {// verifica se um valor existe em uma lista
+		for (int i=0; i < lista.size();i++) {
+			if(lista.get(i)== valor) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static ArrayList<Integer> preencher(int num) {
+		ArrayList<Integer> lista = new ArrayList<>();
+		for (int i=0; i < num ; i++) { 
+			int numGerado = (int)(Math.random()*20+1);
+			lista.add(numGerado);
+		}
+		
+		return lista;
+	}
+	
+
+} // FIM CLASSE
